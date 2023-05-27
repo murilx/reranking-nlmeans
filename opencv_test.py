9440af9b577d55f5663bd274c189586478fbf3b9
Murilo Geraldini <murilogeraldini@unesp.br>
2023-05-27 04:56:05 -0300

    Creation of the opencv_test.py file
    New test file containing the test for the OpenCV implementation of the
    Non-Local Means algorithm.

---
+++ opencv_test.py
@@ -0,0 +1,50 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from skimage.util import random_noise, img_as_ubyte
+from skimage.restoration import estimate_sigma
+from skimage.metrics import peak_signal_noise_ratio as psnr
+from skimage.metrics import structural_similarity as ssim
+from skimage.metrics import mean_squared_error as mse
+import cv2 as cv
+
+import sys
+from time import time
+
+import parameters
+
+hW = parameters.hW
+hP = parameters.hP
+sig = parameters.sig
+h = 20
+
+
+def main():
+    # Load image and synthetize Nakagami-Rayleigh noise of parameter L
+    # im = plt.imread(sys.argv[1]).astype('float')
+    im = cv.imread(sys.argv[1], cv.IMREAD_GRAYSCALE)
+    im_nse = img_as_ubyte(random_noise(im, var=sig ** 2))
+
+    # estimate the noise standard deviation from the noisy image
+    sigma_est = np.mean(estimate_sigma(im_nse, channel_axis=-1))
+    print('estimated noise standard deviation =', sigma_est)
+
+    # Run filtering
+    start_time = time()
+    im_fil = cv.fastNlMeansDenoising(im_nse, None, h, hP, 2*hW+1)
+    print("Time spent: ", time() - start_time)
+
+    # Show results
+    plt.figure()
+    plt.subplot(1, 2, 1)
+    plt.imshow(im_nse, cmap='gray')
+    plt.subplot(1, 2, 2)
+    plt.imshow(im_fil, cmap='gray')
+    plt.show()
+
+    # Evaluate
+    print("PSNR: ", psnr(im, im_fil, data_range=im_fil.max() - im_fil.min()))
+    print("SSIM: ", ssim(im, im_fil, data_range=im_fil.max() - im_fil.min()))
+    print("MSE:  ", mse(im, im_fil))
+    
+if __name__ == '__main__':
+    main()
