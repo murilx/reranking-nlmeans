00f9715d1b5e13171ee1e9c277bee88b66f1a670
Murilo Geraldini <murilogeraldini@unesp.br>
2023-05-30 12:01:01 -0300

    Change random_noise to cv.randn

--- opencv_test.py
+++ opencv_test.py
@@ -15,14 +15,20 @@ import parameters
 hW = parameters.hW
 hP = parameters.hP
 sig = parameters.sig
+sig = 50
+mean = 0.0
 h = 45
 
 
 def main():
-    # Load image and synthetize Nakagami-Rayleigh noise of parameter L
-    # im = plt.imread(sys.argv[1]).astype('float')
+    # Load the image and add noise to it
     im = cv.imread(sys.argv[1], cv.IMREAD_GRAYSCALE)
-    im_nse = img_as_ubyte(random_noise(im, var=sig ** 2))
+    noise = np.copy(im)
+    cv.randn(noise, mean, sig)
+    im_nse = cv.add(im, noise)
+    
+    # im = plt.imread(sys.argv[1]).astype('float')
+    # im_nse = img_as_ubyte(random_noise(im, var=sig ** 2))
 
     # estimate the noise standard deviation from the noisy image
     sigma_est = np.mean(estimate_sigma(im_nse, channel_axis=-1))
