1f0e4fdccf31e8914cf3fd290d3b3dbba4879e93
Murilo Geraldini <murilogeraldini@unesp.br>
2023-08-02 17:00:41 -0300

    Addition of old udlf denoise method
    This code needs to be changed because it does not work properly!

--- nlmeans/nlmeans_udlf.py
+++ nlmeans/nlmeans_udlf.py
@@ -104,7 +104,19 @@ def nlmeans_udlf(ima_nse, hW, hP, tau, sig, shape, num_weights=None):
     sum_wI = np.zeros((M, N))
     new_ranked_lists = ranked_lists # TEMPORARY for tests only
 
-    # TODO: FIND A METHOD OF DENOISE GIVEN THE NEW RANKED LIST
+    for pos in range(new_ranked_lists.shape[0]):
+        # Get image coordinates giving the ranked list position
+        ix = pos // M
+        iy = pos % M
+
+        # Get the indices of every weight
+        # excluding the last `num_weights` of the list
+        new_w_names = new_ranked_lists[pos, :num_weights]
+        weight_indices = np.where(w_names[ix, iy, :num_weights] == new_w_names[:, None])[1]
+
+        # Calculate the desnoised value of each pixel
+        sum_wI[ix, iy] = np.sum(ima_nse[ix, iy] * w_values[ix, iy, weight_indices])
+        sum_w[ix, iy] = np.sum(w_values[ix, iy, weight_indices])
     
     ima_fil = sum_wI / sum_w
     return ima_fil
