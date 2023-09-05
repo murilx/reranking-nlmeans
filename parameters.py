b8c1858c83b9c66e9f9cee2d186754d1834c62d0
Murilo Geraldini <murilo.geraldini@unesp.br>
2023-09-05 16:42:12 -0300

    Deletion of unnecessary files

--- parameters.py
+++
@@ -1,7 +0,0 @@
-hW = 10    # Window size
-hP = 3     # Patch size
-tau = 0.15 # Contribution of similarity on noisy-data
-T = 0.15   # Contribution of similarity on pre-estimated data
-L = 3      # Number of look
-N = 4      # Number of iteration
-sig = 0.1  # Standard deviation of the gaussian noise
